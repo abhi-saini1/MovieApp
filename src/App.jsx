b290fd153962542ddafdf105c0822304398539c4
abhi-saini1 <abhishek977933@gmail.com>
2024-02-07 13:56:01 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useRef} from 'react'
 import { BrowserRouter,Routes,Route} from 'react-router-dom'
 import Home from './Pages/Home/Home'
 import Header from './Components/Header'
@@ -9,17 +9,21 @@ import MovieDetail from './Pages/MoviesDetails/MovieDetail'
 import Discover from './Pages/Discover/Discover'
 import Search from './Pages/Search/Search'
 const App = () => {
+
+ 
   return (
     <>
       <BrowserRouter>
         <MovieProvider>
         <Header/>
         <Routes>
+         
           <Route path='/' element={<Home/>}/> 
           <Route path='/discover' element={<Discover/>}/> 
           <Route path='/movies' element={<Movies/>}/> 
           <Route path='/moviesdetail/:id' element={<MovieDetail/>}/> 
           <Route path='/search' element={<Search/>}/> 
+          
         </Routes> 
         <Footer/>
         </MovieProvider>
